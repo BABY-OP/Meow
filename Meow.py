4527c7f42c09ed57b6cd0c70744b82cfd2c6fc24
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-10-18 14:30:15 +0530

    Update Meow.py

--- Meow.py
+++ Meow.py
@@ -1,1 +1,32 @@
+from pyrogram import idle
+from pytgcalls import idle as pidle
+import asyncio
+from config import (app, hl, TIMEZONE, LOGS_CHANNEL, MONGO_DB, SUDO_USERS, call_py )
+
+
+async def main():
+    print("STARTING  CLIENT")
+    await app.start()
+    print("STARTING PYTGCALLS CLIENT")
+    await call_py.start()
+    print(
+        """
+    ------------------------
+   | Meow Actived! |
+    ------------------------
+"""
+    )
+    await idle()
+    await pidle()
+    print("STOPPING USERBOT")
+    await app.stop()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
+
+
+
+
 
